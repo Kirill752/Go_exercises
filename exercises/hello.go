88c83c866c5008756d7083feee9c4c37a7b32d95
Kirill Latyshov <kil.real.latexshow@gmail.com>
2024-12-15 13:12:46 +0300

    Разбираюсь с импортом пакетов

---
+++ exercises/hello.go
@@ -0,0 +1,5 @@
+package exercises
+
+func Greet(name string) string {
+	return "Hello" + name
+}
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"~/Starting_GOLang/exercises"
+)
+
+func main() {
+	fmt.Println(exercises.Greet("GO"))
+}
