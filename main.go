57ab44eccaa64d813c3164e2bcfbd168fe4de1f9
Kirill Latyshov <kil.real.latexshow@gmail.com>
2024-12-19 18:00:47 +0300

    Попытался разобраться с устройством памяти в go. Особо ничего не понял

--- main.go
+++ main.go
@@ -2,21 +2,16 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func main() {
-	fmt.Println("When's Saturday?")
-	today := time.Now().Weekday()
-	fmt.Println(time.Now())
-	switch time.Saturday {
-	case today + 0:
-		fmt.Println("Today.")
-	case today + 1:
-		fmt.Println("Tomorrow.")
-	case today + 2:
-		fmt.Println("In two days.")
-	default:
-		fmt.Println("Too far away.")
-	}
+	i, j := 1, 270
+
+	p := &i
+	fmt.Printf("The type of i = %d is %T \n", i, i)
+	fmt.Printf("The type of j = %d is %T \n", j, i)
+	fmt.Printf("The adress of i = %d is %d \n", i, p)
+	fmt.Printf("The adress of j = %d is %d \n", j, &j)
+	fmt.Println(p)
+	fmt.Println(&j)
 }
